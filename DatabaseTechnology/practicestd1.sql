b171f919b31dc89536b78d9cb30bda5c43988a7c
shaileshjaunjal <93429245+shaileshjaunjal@users.noreply.github.com>
2021-11-25 10:41:37 +0530

    Add files via upload

---
+++ DatabaseTechnology/practicestd1.sql
@@ -0,0 +1,13 @@
+use practicestd;
+
+delete from std1 where Sroll=10001;
+
+select * from std1;
+
+delete from std1 where Sroll=10001;
+
+select * from std1;
+
+update std1 set marks=60 where Sroll=6;
+
+
